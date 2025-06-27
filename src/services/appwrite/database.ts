bc9074bb229c6dfc2accae36504ff1dfd474f342
Kunal Gupta <guptakunalx@gmail.com>
2025-06-27 18:38:00 +0530

    feat: Add DatabaseService and StorageService for handling posts and file uploads

---
+++ src/services/appwrite/database.ts
@@ -0,0 +1,113 @@
+import { Client, Databases, Query } from "appwrite";
+
+import { config } from "../../config";
+
+type CreatePostParams = {
+  title: string;
+  slug: string;
+  featuredImage: string;
+  description: string;
+  status: string;
+  content: string;
+};
+
+type TUpdatePostParam = {
+  title: string;
+  featuredImage: string;
+  description: string;
+  status: string;
+  content: string;
+};
+
+class DatabaseService {
+  client = new Client();
+  database: Databases;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+    this.database = new Databases(this.client);
+  }
+
+  async createPost({
+    title,
+    slug,
+    featuredImage,
+    description,
+    status,
+    content,
+  }: CreatePostParams) {
+    try {
+      return await this.database.createDocument(
+        config.appwriteDatabaseId,
+        config.appwriteCollectionId,
+        slug,
+        { title, featuredImage, description, status, content },
+      );
+    } catch (error) {
+      console.log("DatabaseService :: createPost :: error ::", error);
+      throw error;
+    }
+  }
+
+  async getPost(slug: string) {
+    try {
+      return this.database.getDocument(
+        config.appwriteDatabaseId,
+        config.appwriteCollectionId,
+        slug,
+      );
+    } catch (error) {
+      console.log("DatabaseService :: getPost :: error ::", error);
+      throw error;
+    }
+  }
+
+  async getPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return this.database.listDocuments(
+        config.appwriteDatabaseId,
+        config.appwriteCollectionId,
+        queries,
+      );
+    } catch (error) {
+      console.log("DatabaseService :: getPosts :: error ::", error);
+      throw error;
+    }
+  }
+
+  async updatePost(
+    { title, featuredImage, description, status, content }: TUpdatePostParam,
+    slug: string,
+  ) {
+    try {
+      return this.database.updateDocument(
+        config.appwriteDatabaseId,
+        config.appwriteCollectionId,
+        slug,
+        { title, featuredImage, description, status, content },
+      );
+    } catch (error) {
+      console.log("DatabaseService :: updatePost :: error ::", error);
+      throw error;
+    }
+  }
+
+  async deletePost(slug: string) {
+    try {
+      return this.database.deleteDocument(
+        config.appwriteDatabaseId,
+        config.appwriteCollectionId,
+        slug,
+      );
+    } catch (error) {
+      console.log("DatabaseService :: deletePost :: error ::", error);
+      throw error;
+    }
+  }
+}
+
+const databaseService = new DatabaseService();
+
+export { databaseService };
---
+++ src/services/appwrite/storage.ts
@@ -0,0 +1,51 @@
+import { Client, Storage, ID } from "appwrite";
+
+import { config } from "../../config";
+
+class StorageService {
+  client = new Client();
+  storage: Storage;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+    this.storage = new Storage(this.client);
+  }
+
+  async uploadFile(file: File) {
+    try {
+      return this.storage.createFile(
+        config.appwriteBucketId,
+        ID.unique(),
+        file,
+      );
+    } catch (error) {
+      console.log("StorageService :: uploadFile :: error ::", error);
+      throw error;
+    }
+  }
+
+  async deleteFile(fileId: string) {
+    try {
+      this.storage.deleteFile(config.appwriteBucketId, fileId);
+      return true;
+    } catch (error) {
+      console.log("StorageService :: deleteFile :: error ::", error);
+      return false;
+    }
+  }
+
+  async getFilePreview(fileId: string) {
+    try {
+      return this.storage.getFileView(config.appwriteBucketId, fileId);
+    } catch (error) {
+      console.log("StorageService :: getFilePreview :: error ::", error);
+      throw error;
+    }
+  }
+}
+
+const storageService = new StorageService();
+
+export { storageService };
