fbd951f83c01265bc59ef2b7140a44e1151ef1bc
Kunal Gupta <guptakunalx@gmail.com>
2025-07-02 13:29:18 +0530

    fix: Correct typo in header title from "Dev Dairy" to "Dev Diary"

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -40,7 +40,7 @@ function Header() {
           <Link to="/">
             <h1 className="flex items-center space-x-1">
               <span className="animate-pulse text-2xl font-black text-slate-900 uppercase">
-                Dev Dairy
+                Dev Diary
               </span>
               <span className="text-base">📝</span>
             </h1>
@@ -57,8 +57,7 @@ function Header() {
                     to={navItem.path}
                     className={({ isActive }) =>
                       `rounded-md px-4 py-2 font-medium text-slate-700 transition-colors duration-200 outline-none hover:bg-slate-200 focus:bg-slate-200 focus:ring-1 focus:ring-slate-400 focus:ring-offset-1 ${isActive ? "bg-slate-200" : ""}`
-                    }
-                  >
+                    }>
                     {navItem.name}
                   </NavLink>
                 </li>
